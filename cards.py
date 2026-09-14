4891bc132a89b8a8d6bf69406dddac2f891ffde9
mgnamdan <danny.ai.hub@gmail.com>
2026-09-14 11:10:54 -0400

    created card

---
+++ cards.py
@@ -0,0 +1,8 @@
+class Card:
+
+    def __init__(self, rank="Two", suit="Clubs"):
+        self.rank = rank
+        self.suit = suit
+
+    def __repr__(self):
+        return f"{self.rank} of {self.suit}"
---
+++ main.py
@@ -0,0 +1,22 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# HELPER FUNCTIONS AND IMPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from cards import Card
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MAIN FUNCTION DEFINITION
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def main():
+    testCard = Card("Ace", "Spades")
+    print(testCard)
+    testCard2 = Card()
+    print(testCard2)
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MAIN FUNCTION CALL
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+main()
