cad38454a830cb3c896148bafd81a3feda65668d
mgnamdan <danny.ai.hub@gmail.com>
2026-09-16 09:46:49 -0400

    add players start

--- cards.py
+++ cards.py
@@ -4,6 +4,16 @@ class Card:
         self.rank = rank
         self.suit = suit
 
+
     def __repr__(self):
         return f"{self.rank} of {self.suit}"
+
     
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        if self.rank != other.rank:
+            return False
+        if self.suit != other.suit:
+            return False
+        return True
